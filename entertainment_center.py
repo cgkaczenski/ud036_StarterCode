7f38308039ffd08960ddbaf0484e5b12f8bac9d1
cgkaczenski <kzpulaski@gmail.com>
2017-08-31 22:37:04 -0500

    style: Add comments and PEP8 style

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,31 @@
 import fresh_tomatoes as ft
 import media
 
-mulholland = media.Movie("Mulholland Drive", "https://upload.wikimedia.org/wikipedia/en/0/0f/Mulholland.png", "https://www.youtube.com/watch?v=jbZJ487oJlY")
-eyes = media.Movie("Eyes Wide Shut", "https://upload.wikimedia.org/wikipedia/en/8/87/Eyes_Wide_Shut_poster.jpg", "https://www.youtube.com/watch?v=_0sJwdgB2Q8")
+mulholland = media.Movie(
+    "Mulholland Drive",
+    "https://upload.wikimedia.org/wikipedia/en/0/0f/Mulholland.png",
+    "https://www.youtube.com/watch?v=jbZJ487oJlY")
 
-movies = [mulholland, eyes]
+eyes = media.Movie(
+    "Eyes Wide Shut",
+    "https://upload.wikimedia.org/wikipedia/en/8/87/Eyes_Wide_Shut_poster.jpg",
+    "https://www.youtube.com/watch?v=_0sJwdgB2Q8")
+
+vertigo = media.Movie(
+    "Vertigo",
+    "https://upload.wikimedia.org/wikipedia/commons/7/75/Vertigomovie_restoration.jpg",
+    "https://www.youtube.com/watch?v=trDqSL_RAsY")
+
+prestige = media.Movie(
+    "The Prestige",
+    "https://upload.wikimedia.org/wikipedia/en/d/d2/Prestige_poster.jpg",
+    "https://www.youtube.com/watch?v=o4gHCmTQDVI")
+
+day = media.Movie(
+    "Day for Night",
+    "https://upload.wikimedia.org/wikipedia/en/2/20/La_Nuit_oscar.jpg",
+    "https://www.youtube.com/watch?v=DKoEVBHMp3s")
+
+movies = [mulholland, eyes, vertigo, prestige, day]
 
 ft.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,4 +1,12 @@
 class Movie(object):
+	"""Stores the movie data.
+
+    Attributes:
+        title: String of movie title.
+        poster_image_url: String of poster url image.
+        trailer_youtube_url: String of youtube trailer
+        url.
+    """
 
     def __init__(self, title, poster, trailer):
         self.title = title
